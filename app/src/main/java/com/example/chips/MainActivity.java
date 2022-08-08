92b4d4240f2fc578070432d88ddfd028722fc13b
Eddie Coertze <johndoe@example.comeddiecoertze@gmail.com>
2022-08-08 13:28:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chips/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.chips;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ChipGroup cg = findViewById(R.id.maps_chip_group);
+
+        cg.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull ChipGroup group, @NonNull List<Integer> checkedIds) {
+
+                int selectedChip = checkedIds.get(0);
+
+                Chip chip = cg.findViewById(selectedChip);
+
+                if (chip != null)
+                    Toast.makeText(MainActivity.this, "Chip: " + chip.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
